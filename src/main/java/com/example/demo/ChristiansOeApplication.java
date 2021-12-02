ec93a5a960faa6af9ab68771882af23baa79056a
Ayo <ayokrohn@gmail.com>
2021-12-02 11:40:41 +0100

    index.html file added

---
+++ src/main/java/com/example/demo/ChristiansOeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristiansOeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChristiansOeApplication.class, args);
+    }
+
+}
