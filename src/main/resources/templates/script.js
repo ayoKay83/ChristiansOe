ae3a2edd0e6111ad622db82580c53fe1099b3310
samavia ahmad <sama0641@stud.kea.dk>
2021-12-05 21:30:18 +0100

    web plugin tilføjet

---
+++ src/main/resources/templates/script.js
@@ -0,0 +1,9 @@
+
+//metode til find din position tager i mod 2 parameter
+//prøv at inspect page og se lang og long for din position
+//så akkurat som muligt
+//array der tager imod koordinaterne
+
+
+
+
