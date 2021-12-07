b3d42eff3bb6f0c91b19d5215e70bd37d8f517da
LingDingLauPau <kasperjakob@me.com>
2021-12-07 02:28:42 +0100

    leaflet test map med modal

--- src/main/java/com/example/demo/models/Location.java
+++ src/main/java/com/example/demo/models/Location.java
@@ -1,19 +1,64 @@
 package com.example.demo.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table
 public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+    @Column(name="name")
     private String name;
+    @Column(name="xpos")
     private double xpos;
+    @Column(name="ypos")
     private double ypos;
-    private Quiz quiz;
 
 
 
+    public Location() {
+    }
+
+
+    public Location(int id, String name, double xpos, double ypos) {
+        this.id = id;
+        this.name = name;
+        this.xpos = xpos;
+        this.ypos = ypos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getXpos() {
+        return xpos;
+    }
+
+    public void setXpos(double xpos) {
+        this.xpos = xpos;
+    }
+
+    public double getYpos() {
+        return ypos;
+    }
+
+    public void setYpos(double ypos) {
+        this.ypos = ypos;
+    }
 
 }
-
-//-  id : int
-//- name : String
-//- xpos : double
-//- ypos : double
-//- quiz : Quiz
