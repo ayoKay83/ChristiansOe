37b0ed1c88bb33100a451f68eb9968b58115eab0
LingDingLauPau <kasperjakob@me.com>
2021-12-16 14:42:44 +0100

    uml

--- src/main/java/com/example/demo/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/exceptions/ResourceNotFoundException.java
@@ -3,6 +3,7 @@ package com.example.demo.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+// Taget
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
